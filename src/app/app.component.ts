f560800f86e0a2916119cced5723932c16a0b125
Anton Chernous (DevExpress) <chernous.anton@corp.devexpress.com>
2020-11-10 16:48:17 +0300

    Add input value to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,12 +10,26 @@ export class AppComponent {
   title = 'my-app';
 
   userText: string = '';
+  public userList: Array<string | number> = [];
 
-  public clickBtn() {
-    const printAndClear = () => {
-      console.log(this.userText);
-      this.userText = '';
-    }
-    return printAndClear();
+  addText() {
+    this.userList.push(this.userText);
+    console.log(this.userList);
   }
+
+  removeText(index: number) {
+    if(index > -1) {
+      this.userList.splice(index, 1);
+      
+    }
+    console.log(this.userList);
+   }
+
+  // public clickBtn() {
+  //   const printAndClear = () => {
+  //     console.log(this.userText);
+  //     this.userText = '';
+  //   }
+  //   return printAndClear();
+  // }
 }
