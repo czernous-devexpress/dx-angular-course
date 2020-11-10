5b5fe4decd91e9741a0df6e3f228054198b6dde1
Anton Chernous (DevExpress) <chernous.anton@corp.devexpress.com>
2020-11-10 19:18:28 +0300

    used both ngClass and ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-
+// add or remove text, checkbox, etc
   userText: string = '';
   public userList: Array<string | number> = [];
 
@@ -24,10 +24,25 @@ export class AppComponent {
     }
     console.log(this.userList);
    }
-   isShow = false;
+   visible : boolean = false;
    toggleDisplay() {
-    this.isShow = !this.isShow;
+    this.visible = !this.visible;
   }
+// toggle color
+
+toggleRed : boolean = true;
+toggleGreen : boolean = false;
+toggleColor() {
+  this.toggleRed = !this.toggleRed;
+}
+toggleSecondColor() {
+  !this.toggleGreen === true ?  this.toggleGreen = true : this.toggleGreen = false;
+}
+public getStyle() {
+  let green = {backgroundColor: '#26de8b'};
+  let red = {backgroundColor: '#b50719'};
+  return this.toggleGreen ? green : red;
+}
   // public clickBtn() {
   //   const printAndClear = () => {
   //     console.log(this.userText);
