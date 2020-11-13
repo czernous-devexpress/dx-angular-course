af3ac77942c9b2b10b0831b87574b02f74328a84
Anton Chernous (DevExpress) <chernous.anton@corp.devexpress.com>
2020-11-13 19:36:23 +0300

    working with DataSource and CustomStore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,42 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 import { Component } from '@angular/core';
-
+import CustomStore from "devextreme/data/custom_store";
+import DataSource from "devextreme/data/data_source";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  store: CustomStore;
+  dataSource: DataSource;
+
   title = 'DX-TEST-APP';
 
-  dxTextBoxValue = '';
+  catchInput = '';
 
-  isPopupVisible = false;
-  
-  displayText() {
-    this.isPopupVisible = true;
+  countries: { id: number, name: string }[] = [
+    {id: 0, name: 'Albania'},
+    {id: 1, name: 'Armenia'},
+    {id: 2, name: 'Belarus'},
+    {id: 3, name: 'Bahrain'},
+    {id: 4, name: 'Canada'},
+    {id: 5, name: 'Cambodia'}
+  ]
+  constructor() {
+    this.store = new CustomStore({
+        load: () => {
+           return this.countries;
+            
+        },
+
+    })
+    this.dataSource = new DataSource({
+        store: this.store,
+
+    });
   }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DxButtonModule, DxPopupModule, DxTextBoxModule } from 'devextreme-angular';
+import { DxButtonModule, DxPopupModule, DxSelectBoxModule, DxTextBoxModule } from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
 import { PopupContentComponent } from './popup-content/popup-content.component';
 
 @NgModule({
   declarations: [AppComponent, PopupContentComponent],
-  imports: [BrowserModule, FormsModule, DxButtonModule, DxTextBoxModule, DxPopupModule],
+  imports: [BrowserModule, FormsModule, DxButtonModule, DxTextBoxModule, DxPopupModule, DxSelectBoxModule],
   providers: [],
   bootstrap: [AppComponent],
 })
